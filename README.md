3bd999571f9a9a5bf39ff82572eeb79fa4474bc9
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-09-16 12:05:10 -0400

    update anki word

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Getting Started with Create React App
 
+https://www.youtube.com/watch?v=5Vp4RVLNo3c
+
 In this tutorial, I've explained how to turn Google API into REST API and use it with React application.
 
 https://www.freecodecamp.org/news/react-and-googlesheets/
+
+https://docs.sheet.best/#generating-your-rest-api
+
+空の Google spread sheet 作成
+anyone with the link に変更
+editor に変更
+copy link
+
+https://sheet.best/
+login
+click connection
+Name: Google API
+URL: from "copy link"
+
+click detail from "Google API"
+copy link
+
+add in your axios at React
+
+https://docs.google.com/spreadsheets/d/165zCzZbMX_rCmxadjXt8KnjFH2oDWE-sqObPjGIOuIE/edit?usp=sharing
--- src/App.js
+++ src/App.js
@@ -1,68 +1,190 @@
-import React, { useState } from 'react';
-import { Button, Form, Container, Header } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, Container, Header, Table } from 'semantic-ui-react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-	const [name, setName] = useState('');
-	const [age, setAge] = useState('');
-	const [salary, setSalary] = useState('');
-	const [hobby, setHobby] = useState('');
+  const [week, setWeek] = useState('');
+  const [date, setDate] = useState('');
+  const [rank, setRank] = useState('');
+  const [word, setWord] = useState('');
+  const [ipa, setIpa] = useState('');
+  const [mean, setMean] = useState('');
+  const [poSpeech, setPoSpeech] = useState('');
+  const [anchor, setAnchor] = useState('');
+  const [test, setTest] = useState('');
+  const [random, setRandom] = useState('');
+  const [order, setOrder] = useState('');
+  // const [name, setName] = useState('');
+  // const [age, setAge] = useState('');
+  // const [salary, setSalary] = useState('');
+  // const [hobby, setHobby] = useState('');
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const [APIdata, setAPIdata] = useState([]);
+  const [refresh, setRefresh] = useState([]);
 
-		const objt = { name, age, salary, hobby };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		axios
-			.post(
-				'https://sheet.best/api/sheets/8abefc9d-5af1-4c54-b1c3-e010462fd81a',
-				objt
-			)
-			.then((response) => {
-				console.log(response);
-			});
-	};
+    const objt = {
+      week,
+      date,
+      rank,
+      word,
+      ipa,
+      mean,
+      poSpeech,
+      anchor,
+      test,
+      random,
+      order,
+    };
 
-	return (
-		<Container fluid className="container">
-			<Header as="h2">React google sheet</Header>
-			<Form className="form">
-				<Form.Field>
-					<label>Name</label>
-					<input
-						placeholder="Enter your Name"
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Age</label>
-					<input
-						placeholder="Enter your Age"
-						onChange={(e) => setAge(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Salary</label>
-					<input
-						placeholder="Enter your Salary"
-						onChange={(e) => setSalary(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Hobby</label>
-					<input
-						placeholder="Enter your Hobby"
-						onChange={(e) => setHobby(e.target.value)}
-					/>
-				</Form.Field>
+    axios
+      .post(
+        `https://sheet.best/api/sheets/d3e272fe-0d6f-44ff-86ec-82d17b9f3667`,
+        objt
+      )
+      .then((response) => {
+        console.log(response);
+        setRefresh(response);
+      });
+  };
 
-				<Button color="blue" type="submit" onClick={handleSubmit}>
-					Submit
-				</Button>
-			</Form>
-		</Container>
-	);
+  useEffect(() => {
+    axios
+      .get(`https://sheet.best/api/sheets/d3e272fe-0d6f-44ff-86ec-82d17b9f3667`)
+      .then((incomingData) => {
+        setAPIdata(incomingData.data);
+      });
+  }, [refresh]);
+
+  return (
+    <Container fluid className="container">
+      <Header as="h2">React google sheet</Header>
+      <Form className="form">
+        <Form.Field>
+          <label>Week</label>
+          <input
+            placeholder="Enter your Week"
+            onChange={(e) => setWeek(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Date</label>
+          <input
+            placeholder="Enter your Date"
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Rank</label>
+          <input
+            placeholder="Enter your Rank"
+            onChange={(e) => setRank(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Word</label>
+          <input
+            placeholder="Enter your Word"
+            onChange={(e) => setWord(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Ipa</label>
+          <input
+            placeholder="Enter your Ipa"
+            onChange={(e) => setIpa(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Mean</label>
+          <input
+            placeholder="Enter your Mean"
+            onChange={(e) => setMean(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>PoSpeech</label>
+          <input
+            placeholder="Enter your PoSpeech"
+            onChange={(e) => setPoSpeech(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Anchor</label>
+          <input
+            placeholder="Enter your Anchor"
+            onChange={(e) => setAnchor(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Test</label>
+          <input
+            placeholder="Enter your Test"
+            onChange={(e) => setTest(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Random</label>
+          <input
+            placeholder="Enter your Random"
+            onChange={(e) => setRandom(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Order</label>
+          <input
+            placeholder="Enter your Order"
+            onChange={(e) => setOrder(e.target.value)}
+          />
+        </Form.Field>
+
+        <Button color="blue" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+
+      <Table fixed>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Week</Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Rank</Table.HeaderCell>
+            <Table.HeaderCell>Word</Table.HeaderCell>
+            <Table.HeaderCell>Ipa</Table.HeaderCell>
+            <Table.HeaderCell>Mean</Table.HeaderCell>
+            <Table.HeaderCell>PoSpeech</Table.HeaderCell>
+            <Table.HeaderCell>Anchor</Table.HeaderCell>
+            <Table.HeaderCell>Test</Table.HeaderCell>
+            <Table.HeaderCell>Random</Table.HeaderCell>
+            <Table.HeaderCell>Order</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {APIdata.map((data) => {
+            return (
+              <Table.Row>
+                <Table.Cell>{data.week}</Table.Cell>
+                <Table.Cell>{data.date}</Table.Cell>
+                <Table.Cell>{data.rank}</Table.Cell>
+                <Table.Cell>{data.word}</Table.Cell>
+                <Table.Cell>{data.ipa}</Table.Cell>
+                <Table.Cell>{data.mean}</Table.Cell>
+                <Table.Cell>{data.poSpeech}</Table.Cell>
+                <Table.Cell>{data.anchor}</Table.Cell>
+                <Table.Cell>{data.test}</Table.Cell>
+                <Table.Cell>{data.random}</Table.Cell>
+                <Table.Cell>{data.order}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </Container>
+  );
 }
 
 export default App;
